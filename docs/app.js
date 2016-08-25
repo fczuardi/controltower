aac9dc80bc7abb7f2230df9e2a18995f1ba11bd3
Fabricio C Zuardi <fabricio@fabricio.org>
2016-08-25 18:48:40 -0300

    Page update.

--- docs/app.js
+++ docs/app.js
@@ -27,7 +27,7 @@ var config = createCommonjsModule(function (module) {
 
 var config$1 = interopDefault(config);
 
-var version = "0.6.7";
+var version = "0.6.8";
 var homepage = "https://github.com/fczuardi/controltower#readme";
 
 function fbGetUserInfo (userFields, send, done) {
