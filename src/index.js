192dedea1108c80f4e40f8b7ebfe63e7c534aabc
Fabricio C Zuardi <fabricio@fabricio.org>
2016-09-02 18:40:55 -0300

    put the slashes again so i can remove the one in my dev environment

--- src/index.js
+++ src/index.js
@@ -58,7 +58,7 @@ const setupModel = {
     },
     effects: {
         fetch: (data, state, send, done) => {
-            send('location:set', { pathname: `${rootPath}b/${data.setupId}` }, done);
+            send('location:set', { pathname: `${rootPath}/b/${data.setupId}` }, done);
         }
     }
 };
@@ -74,7 +74,7 @@ const authWrapper = (loggedView, anonView) => (state, prev, send) => (
 
 console.log('rootPath', rootPath);
 app.router([
-    [`${rootPath}`, authWrapper(dashboardView, mainView)],
+    [`${rootPath}/`, authWrapper(dashboardView, mainView)],
     [`${rootPath}/b/:botId`, authWrapper(setupForm, mainView)]
 ]);
 
