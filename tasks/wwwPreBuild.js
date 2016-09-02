aa12192bd03993a7963f80072c2b901b9accedb3
Fabricio C Zuardi <fabricio@fabricio.org>
2016-09-02 13:00:35 -0300

    include a hash in the js urls to avoid cached urls

--- tasks/wwwPreBuild.js
+++ tasks/wwwPreBuild.js
@@ -1,8 +1,11 @@
 const shelljs = require('shelljs');
-const { mkdir, cp } = shelljs;
+const { mkdir, cp, sed } = shelljs;
+const pkg = require('../package.json');
+const { version } = pkg;
 
 const webDistroPath = 'docs/';
 const libsPath = `${webDistroPath}js/`;
 
 mkdir('-p', libsPath);
 cp('src/index_prod.html', `${webDistroPath}index.html`);
+sed('-i', '.js"', `.js?cachebuster=${version}"`, `${webDistroPath}index.html`);
