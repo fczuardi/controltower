0e6ff6e1f20ca8ac51e56e4dbee8045b5c7a2b63
Fabricio C Zuardi <fabricio@fabricio.org>
2016-09-06 02:53:46 -0300

    make login screen localizeable

--- locales/ptBr.js
+++ locales/ptBr.js
@@ -1,4 +1,9 @@
 module.exports = {
+    login: {
+        title: 'Conecte-se',
+        subtitle: 'Para acessar sua torre de controle é preciso se identificar.',
+        fbSignInButton: 'Acessar com Facebook'
+    },
     dashboard: {
         title: 'Dashboard',
         botUrl: 'Entre o identificador do seu bot (enviado por email)',
