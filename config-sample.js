dc3cd995415494856cfc5de51f07a857009a8094
Fabricio C Zuardi <fabricio@fabricio.org>
2016-09-13 22:23:26 -0300

    Ask for pages_show_list permission on login/signup

--- config-sample.js
+++ config-sample.js
@@ -2,7 +2,7 @@ module.exports = {
     facebook: {
         appId: 'FACEBOOK_APP_ID',
         loginParams: {
-            scope: 'public_profile,email'
+            scope: 'public_profile,email,pages_show_list'
         },
         userFields: 'id,name,email'
     }
